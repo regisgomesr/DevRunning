2c52e70c6de48b83c01b33fd6c0633b05ec4e95c
Regis Ribeiro <regisgomesr@gmail.com>
2020-01-23 12:55:31 -0300

    Adicionando Form no component MyAccount.

--- jwt/src/screens/Restrito/MyAccount.js
+++ jwt/src/screens/Restrito/MyAccount.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ActionCreators from '../../redux/actionCreators'
 import { connect } from 'react-redux'
-import { Button } from 'semantic-ui-react'
+import { Button, Form, Segment } from 'semantic-ui-react'
 
 import timezones from 'moment-timezone/data/meta/latest.json'
 
@@ -17,6 +17,7 @@ class MyAccount extends Component{
             unit: this.props.auth.user.unit,
             timezone: this.props.auth.user.timezone
         })
+        this.props.reset()
     }
 
     handleChange = fieldname => event => {
@@ -38,23 +39,40 @@ class MyAccount extends Component{
             <div>
                 <h1>Minha Conta</h1>
 
-                <select value={this.state.unit} onChange={this.handleChange('unit')}>
-                    <option value='metric'>Métrico (Km)</option>
-                    <option value='imperial'>Imperial (mi)</option>
-                </select>
+                {
+                    this.props.auth.saved && <Segment color='green'>Configurações alteradas com sucesso!</Segment>
+                }
+                {
+                    !this.props.auth.saved && 
+                    <Form>
 
-                <select value={this.state.timezone} onChange={this.handleChange('timezone')}>
-                    {
-                        Object
-                            .keys(timezones.zones)
-                            .map(tz => {
-                                return <option key={tz} value={tz}>{tz}</option>
-                            })
-                    }
-                </select>
+                        <Form.Field>
+                            <label>Unidade de Comprimento</label>
+                            <select value={this.state.unit} onChange={this.handleChange('unit')}>
+                                <option value='metric'>Métrico (Km)</option>
+                                <option value='imperial'>Imperial (mi)</option>
+                            </select>
+                        </Form.Field>
 
-                <Button onClick={this.handleSave}>Salvar</Button>
-                
+                        <Form.Field>
+                            <label>Fuso Horário</label>
+                            <select value={this.state.timezone} onChange={this.handleChange('timezone')}>
+
+                            {
+                            Object
+                                .keys(timezones.zones)
+                                .map(tz => {
+                                    return <option key={tz} value={tz}>{tz}</option>
+                                })
+                            }
+
+                            </select>
+                        </Form.Field>
+
+                        <Button onClick={this.handleSave}>Salvar</Button>
+                        
+                    </Form>
+                }
             </div>
         )
     }
@@ -68,7 +86,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        save: (user) => dispatch(ActionCreators.updateProfileRequest(user))
+        save: (user) => dispatch(ActionCreators.updateProfileRequest(user)),
+        reset: () => dispatch(ActionCreators.updateProfileReset())
     }
 }
 
